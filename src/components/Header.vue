3709e2121eebc41188432d7acd211956cc7dade1
ryanchiang0710 <36843231+ryanchiang0710@users.noreply.github.com>
2022-07-03 14:27:09 +0800

    commit

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,7 @@ import {ArrowRightIcon, ChevronDoubleDownIcon} from '@heroicons/vue/solid'
 <template>
 	<section class="flex flex-col justify-center items-center md:px-20 min-h-screen" :style="{'background-image':'url(https://th.bing.com/th/id/OIP.l-7ci-qyVMc6vvCMlOHBQQHaEo?pid=ImgDet&rs=1)'}">
 		<div class="flex flex-col w-full py-12 px-4 lg:px-0 justify-center items-center">
-			<p class="text-4x1 text-green-800 font-bold">我是<span class="text-blue-400">江承恩</span></p>
+			<p class="text-4x1 text-white font-bold">$Whoami: <span class="text-white">江承恩</span></p>
 			<h1 class="text-6xl items-center justify-center text-white mt-1 font-extrabold">
 				一個喜歡資訊安全的高中生
 			</h1>
