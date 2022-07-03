3b3bd74d676e296a244301479bd4a606c80fac22
ryanchiang0710 <36843231+ryanchiang0710@users.noreply.github.com>
2022-07-03 14:47:59 +0800

    commit

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
 import {ArrowRightIcon, ChevronDoubleDownIcon} from '@heroicons/vue/solid'
 </script>
 <template>
-	<section class="flex flex-col justify-center items-center md:px-20 min-h-screen" :style="{'background-image':'url(https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fgumlet.assettype.com%2Fnationalherald%252F2020-06%252F46805073-cefd-4314-98c7-ab0b121ca103%252FHacking.png%3Frect%3D0%252C0%252C1200%252C675%26auto%3Dformat%252Ccompress%26w%3D1200&f=1&nofb=1)'}">
+	<section class="flex flex-col justify-center items-center md:px-20 min-h-screen" :style="{'background-image':'url(https://www.fmsh.fr/sites/default/files/styles/programme_page_img_principale/public/image/hacking-dea.jpg?itok=5GjSnZwc)'}">
 		<div class="flex flex-col w-full py-12 px-4 lg:px-0 justify-center items-center">
 			<p class="text-4x1 text-white font-bold">$Whoami: <span class="text-white">江承恩</span></p>
 			<h1 class="text-6xl items-center justify-center text-white mt-1 font-extrabold">
@@ -13,13 +13,13 @@ import {ArrowRightIcon, ChevronDoubleDownIcon} from '@heroicons/vue/solid'
 			</p>
 			<a
 				href="#contact"
-				class="mt-3 flex items-center text-xl font-bold pb-2 text-gray-400">
+				class="mt-3 flex items-center text-xl font-bold pb-2 text-blue-400">
 				聯絡我
 				<ArrowRightIcon class="ml-1 w-5 h-5"/>
 			</a>
 		</div>
 
-		<a href="#activities" class="animate-bounce cursor-pointer">
+		<a href="#activities" class="animate-bounce cursor-pointer text-blue-400">
 			<chevron-double-down-icon class="w-16 h-16"/>
 		</a>
 	</section>
